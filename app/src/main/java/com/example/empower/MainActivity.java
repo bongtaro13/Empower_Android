de42ae0e3553d857c82d545c1df268da85b03a70
Bongtaro Hao <bongtaro13@gmail.com>
2020-08-24 12:39:08 +1000

    main activity added with simple UI test

--- app/src/main/java/com/example/empower/MainActivity.java
+++ app/src/main/java/com/example/empower/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
+    // main activity test added
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
