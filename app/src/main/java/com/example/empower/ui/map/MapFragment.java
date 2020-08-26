8e673f32ecbf5db348c5c815cefd09720fcaaf89
Bongtaro Hao <bongtaro13@gmail.com>
2020-08-26 16:44:10 +1000

    fragment_map.xml clear for map display Need to be done: Add google Map SDK for android in this application Transfer SportsVenue object to Json

--- app/src/main/java/com/example/empower/ui/map/MapFragment.java
+++ app/src/main/java/com/example/empower/ui/map/MapFragment.java
@@ -30,16 +30,16 @@ public class MapFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        mapViewModel =
-                ViewModelProviders.of(this).get(MapViewModel.class);
+//        mapViewModel =
+//                ViewModelProviders.of(this).get(MapViewModel.class);
         View root = inflater.inflate(R.layout.fragment_map, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        mapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_dashboard);
+//        mapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 
