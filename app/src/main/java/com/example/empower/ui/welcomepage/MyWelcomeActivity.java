d6ffeb55ab8cf9447c9cb2b8aa858c91f7df7efb
Bongtaro Hao <bongtaro13@gmail.com>
2020-08-31 23:09:50 +1000

    action bar removed in welcome page

--- app/src/main/java/com/example/empower/ui/welcomepage/MyWelcomeActivity.java
+++ app/src/main/java/com/example/empower/ui/welcomepage/MyWelcomeActivity.java
@@ -15,41 +15,41 @@ public class MyWelcomeActivity extends WelcomeActivity {
     @Override
     protected WelcomeConfiguration configuration() {
         return new WelcomeConfiguration.Builder(this)
-                .defaultTitleTypefacePath("Montserrat-Bold.ttf")
-                .defaultHeaderTypefacePath("Montserrat-Bold.ttf")
-
-                .page(new BasicPage(R.drawable.ic_front_desk_white,
-                        "Welcome",
-                        "An Android library for onboarding, instructional screens, and more")
-                        .background(R.color.orange_background)
+                .defaultBackgroundColor(R.color.blue_background)
+                .page(new TitlePage(R.drawable.logo,
+                        "Title")
                 )
-
-                .page(new BasicPage(R.drawable.ic_thumb_up_white,
-                        "Simple to use",
-                        "Add a welcome screen to your app with only a few lines of code.")
+                .page(new BasicPage(R.drawable.logo,
+                        "Header",
+                        "More text.")
                         .background(R.color.red_background)
                 )
-
-                .page(new ParallaxPage(R.layout.parallax_example,
-                        "Easy parallax",
-                        "Supply a layout and parallax effects will automatically be applied")
-                        .lastParallaxFactor(2f)
-                        .background(R.color.purple_background)
+                .page(new BasicPage(R.drawable.logo,
+                        "Lorem ipsum",
+                        "dolor sit amet.")
                 )
-
-                .page(new BasicPage(R.drawable.ic_edit_white,
-                        "Customizable",
-                        "All elements of the welcome screen can be customized easily.")
-                        .background(R.color.blue_background)
-                )
-
                 .swipeToDismiss(true)
-                .exitAnimation(android.R.anim.fade_out)
                 .build();
     }
 
     public static String welcomeKey() {
         return "WelcomeScreen";
+    }
+
+    WelcomeHelper welcomeScreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        welcomeScreen = new WelcomeHelper(this, MyWelcomeActivity.class);
+        welcomeScreen.show(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        welcomeScreen.onSaveInstanceState(outState);
     }
 
 }
