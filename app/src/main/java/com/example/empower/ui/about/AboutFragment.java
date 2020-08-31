1af7695bcdd8780dfb1f0c643d14c309e827d12b
Bongtaro Hao <bongtaro13@gmail.com>
2020-08-31 21:52:11 +1000

    contact us page updated with signal email, more updated on this can be done with discussion

--- app/src/main/java/com/example/empower/ui/about/AboutFragment.java
+++ app/src/main/java/com/example/empower/ui/about/AboutFragment.java
@@ -11,6 +11,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -23,6 +24,7 @@ public class AboutFragment extends Fragment {
     private AboutViewModel aboutViewModel;
     private Button feedbackButton;
     private WebView webView;
+    private TextView contactTextview;
 
     private View root;
 
@@ -33,7 +35,9 @@ public class AboutFragment extends Fragment {
                 ViewModelProviders.of(this).get(AboutViewModel.class);
         root = inflater.inflate(R.layout.fragment_about, container, false);
 
+        contactTextview = root.findViewById(R.id.about_contactdetail_textView);
         feedbackButton = root.findViewById(R.id.about_feedback_button);
+
         feedbackButton.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetJavaScriptEnabled")
             @Override
@@ -44,6 +48,21 @@ public class AboutFragment extends Fragment {
                 //load WebView
                 webView.loadUrl("https://docs.google.com/forms/d/e/1FAIpQLSdtNt9-3_nZlKA4ZjP0uaClP6-B1Ok9EoU-qlcYuS0oOxWWkw/viewform");
                 feedbackButton.setVisibility(View.GONE);
+                contactTextview.setVisibility(View.GONE);
+                webView.setOnKeyListener(new View.OnKeyListener() {
+                    @Override
+                    public boolean onKey(View v, int keyCode, KeyEvent event) {
+                        if (event.getAction() == KeyEvent.ACTION_DOWN){
+                            if (keyCode == KeyEvent.KEYCODE_BACK){
+                                webView.goBack();
+                                return true;
+
+                            }
+
+                        }
+                        return false;
+                    }
+                });
 
             }
         });
