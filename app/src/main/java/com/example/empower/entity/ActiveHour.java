b6d5a37119337a4bd7a18b23ec4fffc5c37d8185
Bongtaro Hao <bongtaro13@gmail.com>
2020-10-21 22:17:52 +1100

    fix Google Place API for opening time in a selected place add default time if there is no information called back from Place API

--- app/src/main/java/com/example/empower/entity/ActiveHour.java
+++ app/src/main/java/com/example/empower/entity/ActiveHour.java
@@ -3,16 +3,20 @@ package com.example.empower.entity;
 import java.util.ArrayList;
 import java.util.Random;
 
+
+
+// default active if no information of active from Google Place API
+
 public class ActiveHour {
     private ArrayList<String> activeHour;
 
     public ActiveHour() {
         this.activeHour = new ArrayList<>();
-        this.activeHour.add("Active: 9:30am–4:30pm");
-        this.activeHour.add("Active: 9:00am–5:00pm");
-        this.activeHour.add("Active: 10:30am–5:00pm");
-        this.activeHour.add("Active: 10:30am–4:30pm");
-        this.activeHour.add("Active: 10:30am–4:30pm");
+        this.activeHour.add("9:30am–4:30pm");
+        this.activeHour.add("9:00am–5:00pm");
+        this.activeHour.add("10:30am–5:00pm");
+        this.activeHour.add("10:30am–4:30pm");
+        this.activeHour.add("10:30am–4:30pm");
     }
 
     public ArrayList<String> getActiveHour() {
--- app/src/main/java/com/example/empower/ui/map/VenueDetailPopup.java
+++ app/src/main/java/com/example/empower/ui/map/VenueDetailPopup.java
@@ -3,6 +3,7 @@ package com.example.empower.ui.map;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -23,20 +24,43 @@ import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.empower.R;
 import com.example.empower.database.LikedVenue;
+import com.example.empower.entity.ActiveHour;
 import com.example.empower.entity.Venue;
 import com.example.empower.ui.about.AboutViewModel;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.maps.StreetViewPanoramaOptions;
 import com.google.android.gms.maps.StreetViewPanoramaView;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.OpeningHours;
+import com.google.android.libraries.places.api.model.PhotoMetadata;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FetchPhotoRequest;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
+import com.google.android.libraries.places.api.net.PlacesClient;
 import com.like.LikeButton;
 import com.like.OnLikeListener;
 import com.lxj.xpopup.core.BottomPopupView;
 import com.lxj.xpopup.util.XPopupUtils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 
 public class VenueDetailPopup extends BottomPopupView {
@@ -60,8 +84,12 @@ public class VenueDetailPopup extends BottomPopupView {
 
     private Boolean switchFlag;
 
+    private ActiveHour active_hours = new ActiveHour();
+
 
     private Bundle venueDetail;
+
+    private String selectedPlaceID = "ChIJdX6CmVhE1moRjNjM6yDQJBs";
 
 
     public VenueDetailPopup(@NonNull Context context, Bundle venueInformationBundle) {
@@ -80,7 +108,6 @@ public class VenueDetailPopup extends BottomPopupView {
         super.onCreate();
 
 
-
         venueName = findViewById(R.id.venueName_map_popup);
         closeButton = findViewById(R.id.close_map_popup);
         accessibleSwtih = findViewById(R.id.accessible_switch);
@@ -93,12 +120,12 @@ public class VenueDetailPopup extends BottomPopupView {
         accessibleSwtih.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
+                if (isChecked) {
                     SharedPreferences accessibleFlag = getContext().getSharedPreferences("switchFlag", Context.MODE_PRIVATE);
                     SharedPreferences.Editor editor = accessibleFlag.edit();
                     editor.putBoolean("flag", true);
                     editor.apply();
-                }else {
+                } else {
                     SharedPreferences accessibleFlag = getContext().getSharedPreferences("switchFlag", Context.MODE_PRIVATE);
                     SharedPreferences.Editor editor = accessibleFlag.edit();
                     editor.putBoolean("flag", false);
@@ -106,7 +133,6 @@ public class VenueDetailPopup extends BottomPopupView {
                 }
             }
         });
-
 
 
         venueType = findViewById(R.id.venueType_map_popup);
@@ -132,17 +158,77 @@ public class VenueDetailPopup extends BottomPopupView {
             venueType.setText(selectLikedVenue.getType());
             venueAddress.setText(selectLikedVenue.getAddress());
             venueSuburb.setText(selectLikedVenue.getSuburb() + " " + selectLikedVenue.getPostcode());
-            venueBusinessCategory.setText(selectLikedVenue.getBusinessCategory().replace(","," "));
+            venueBusinessCategory.setText(selectLikedVenue.getBusinessCategory().replace(",", " "));
             venueLga.setText(selectLikedVenue.getLga());
-            if (venueType.equals("sport venue")){
+
+            if (selectLikedVenue.getType().equals("sport venue")) {
                 activeHour.setVisibility(VISIBLE);
-                activeHour.setText(activeHourString);
-            }else {
-                activeHour.setVisibility(GONE);
+                activeHour.setText(active_hours.getAciveHours());
+            } else {
+                activeHour.setVisibility(INVISIBLE);
             }
         }
 
 
+        // Instantiate the RequestQueue
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        String methodPath = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=";
+        String inpuType = "&inputtype=textquery&fields=place_id,rating&locationbias=circle:2000@";
+        String placeKey = "&key=AIzaSyBenJ8IiMcO7vlKFYcZXb9WhKWuTQEJeo4";
+        String venueNameInput = selectLikedVenue.getName().replace(" ", "%20");
+        String venueLat = selectLikedVenue.getLatitude();
+        String venueLng = selectLikedVenue.getLongitude();
+
+        String url = methodPath + venueNameInput + inpuType + venueLat + "," + venueLng + placeKey;
+
+        // Request a string response from the provided URL
+        StringRequest placeIDRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String result) {
+                try {
+                    JSONObject responseResult = new JSONObject(result);
+                    JSONArray candidatesJsonArray = responseResult.getJSONArray("candidates");
+                    JSONObject firstCandidate = (JSONObject) candidatesJsonArray.get(0);
+                    selectedPlaceID = firstCandidate.getString("place_id");
+// Initialize the SDK
+                    Places.initialize(getContext().getApplicationContext(), "AIzaSyBenJ8IiMcO7vlKFYcZXb9WhKWuTQEJeo4");
+
+                    // Create a new PlacesClient instance
+                    PlacesClient placesClient = Places.createClient(getContext());
+
+                    // Define a Place ID.
+                    String placeId = selectedPlaceID;
+
+                    // Specify fields. Requests for photos must always have the PHOTO_METADATAS field.
+                    final List<Place.Field> fields = Collections.singletonList(Place.Field.PHOTO_METADATAS);
+
+                    // Get a Place object (this example uses fetchPlace(), but you can also use findCurrentPlace())
+                    final FetchPlaceRequest placeRequest = FetchPlaceRequest.newInstance(placeId, fields);
+
+                    placesClient.fetchPlace(placeRequest).addOnSuccessListener((response) -> {
+                        final Place place = response.getPlace();
+
+                        OpeningHours openingHours = place.getOpeningHours();
+                        if (openingHours != null) {
+                            activeHour.setText(openingHours.toString());
+                        }
+                    });
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(placeIDRequest);
 
 
         closeButton.setOnClickListener(new OnClickListener() {
@@ -152,7 +238,6 @@ public class VenueDetailPopup extends BottomPopupView {
                 dismiss();
             }
         });
-
 
 
     }
